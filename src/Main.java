70b37da9edf32600ddde4b64a70143f8589e2f10
Max test <max_dancer94@mail.ru>
2023-01-12 22:52:56 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5_000;
+        int oneMilePrice = 20;
+        int bonusMiles = ticketPrice / oneMilePrice;
+
+        System.out.println(bonusMiles);
+    }
+}
