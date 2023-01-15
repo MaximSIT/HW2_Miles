7c113bc29834aac7c38395eadb4a88d042dcac21
Max test <max_dancer94@mail.ru>
2023-01-15 14:15:42 +0500

    New class service added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int oneMilePrice = 20;
+        int result = cost / oneMilePrice;
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 5_000;
-        int oneMilePrice = 20;
-        int bonusMiles = ticketPrice / oneMilePrice;
-
-        System.out.println(bonusMiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
